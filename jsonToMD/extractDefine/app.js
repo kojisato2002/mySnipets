f90ab620377ee84e0c2e6204a5ac7dae5d221585
Koji Sato <kojisato2002@gmail.com>
2019-04-15 18:44:39 +0900

    フォームを表示するまで

--- jsonToMD/extractDefine/app.js
+++ jsonToMD/extractDefine/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var hello = require('./routes/hello');
+var sendForm = require('./routes/sendForm');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/hello', hello);
+app.use('/sendForm', sendForm);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
