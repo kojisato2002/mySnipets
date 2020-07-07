b7ed85bc2994e8b604c5e088538e1be1bbde3b40
kojisato2002 <kojisato2002@gmail.com>
2020-07-07 21:43:45 +0900

    Sort the information

---
+++ pluginDev/sample/makePluginFolder.bat
@@ -0,0 +1,18 @@
+mkdir src
+cd src
+mkdir image
+mkdir js
+cd js
+echo "customize.js" > customize.js
+echo "mobile-customize.js" > mobile-customize.js
+echo "config.js" > config.js
+cd ..
+mkdir css
+cd css
+echo "customize.css" > customize.css
+echo "mobile-customize.css" > mobile-customize.css
+echo "config.css" > config.css
+cd ..
+mkdir html
+cd html
+echo "config.html" > config.html
